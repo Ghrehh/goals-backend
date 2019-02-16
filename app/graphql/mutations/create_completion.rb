05bbd76f2e706a5d32c541bdf4c9db28b35fe472
Gerard Burns <gjburns9@hotmail.com>
2019-02-16 18:50:15 +0000

    completion delete and create

--- app/graphql/mutations/create_completion.rb
+++ app/graphql/mutations/create_completion.rb
@@ -8,14 +8,14 @@ module Mutations
     argument :goal_id, Int, required: true
     argument :completed_at, GraphQL::Types::ISO8601DateTime, required: true
 
-    field :completion, Types::CompletionType, null: true
+    field :goals, [Types::GoalType], null: false
 
     def resolve(auth:, goal_id:, completed_at:)
       user = Auth.authorized_user_for(**auth.to_h)
       goal = user&.goals&.find(goal_id)
       completion = goal&.completions&.create(completed_at: completed_at)
 
-      { completion: completion }
+      { goals: user&.goals }
     end
   end
 end
