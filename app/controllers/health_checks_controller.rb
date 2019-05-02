650a54200b445234b8baefff6bd90d8d8c9db6f2
Gerard Burns <gjburns9@hotmail.com>
2019-05-02 20:41:47 +0100

    add health check endpoint

---
+++ app/controllers/health_checks_controller.rb
@@ -0,0 +1,5 @@
+class HealthChecksController < ApplicationController
+  def show
+    render json: {}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   post "/gql", to: "graphql#execute"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :health_check, only: [:show]
 end
